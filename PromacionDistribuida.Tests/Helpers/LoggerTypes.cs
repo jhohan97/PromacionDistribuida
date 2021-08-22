e5d97fee6ef7c2e6d451217cd449c35114f821de
Jnorenap <jnorenap@devco.com.co>
2021-08-22 16:05:43 -0500

    Unit Test preparation

---
+++ PromacionDistribuida.Tests/Helpers/LoggerTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromacionDistribuida.Tests.Helpers
+{
+    public  enum  LoggerTypes
+    {
+        Null,
+        List
+    }
+}
